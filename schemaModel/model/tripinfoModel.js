bbec8f73ea4ff74fa4195144b584ad1cd52da3f8
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-07-11 11:02:55 +0545

    Some fields added to tripinfomodel

--- schemaModel/model/tripinfoModel.js
+++ schemaModel/model/tripinfoModel.js
@@ -8,6 +8,42 @@ const tripinfoSchema = new mongoose.Schema({
     description : {
         type : String,
         required : [true,"Description is required"]
+    },
+    image : {
+        data : Buffer,
+        contentType : String
+    },
+    itinerary : {
+        type : String,
+        require : [true]
+    },
+    duration :{
+        type : String,
+        required : true
+    },
+    trip_grade : {
+        type : String,
+        required : true
+    },
+    seasons : {
+        type : String,
+        required : [true]
+    },
+    price : {
+        type : String,
+        required : [true]
+    },
+    cost_includes : {
+        type : String ,
+        required : [true]
+    },
+    cost_excludes : {
+        type : String,
+        required : [true]
+    },
+    overview : {
+        type : String,
+        required : [true]
     }
 },{
     timestamps : true 
