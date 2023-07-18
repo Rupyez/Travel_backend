a9d1292d6712834325f03267d32952dcd7bfeb66
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2023-07-18 12:03:47 +0545

    Added get all , specified, update and delete methods in controller and created routes for those

--- controller/socialmediaController.js
+++ controller/socialmediaController.js
@@ -22,3 +22,49 @@ export const createSocialMedia = catchAsyncError(async(req,res) => {
         data
     })
 })
+
+export const getSpecifiedSocialMedia = catchAsyncError(async(req,res) => {
+    let id = req.params.id
+    let data = await socialmediaService.getSpecifiedSocialMediaService({id})
+    if(!data) {
+        let error = new Error("SocialMedia Page Not Found")
+        error.statusCode = HttpStatus.NOT_FOUND
+        throw error
+    }
+    successResponseData({
+        res : res ,
+        message : "Specified SocialMedia Page is found",
+        statusCode : HttpStatus.OK,
+        data
+    })
+})
+
+export const getAllSocialMedia = catchAsyncError(async(req,res,next) => {
+    let find = {}
+    req.find = find
+    req.service = await socialmediaService.getAllSoicalMediaService
+    next()
+})
+
+export const updateSocialMedia = catchAsyncError(async(req,res,next) => {
+    let body = req.body
+    let id = req.params.id
+    let data = await socialmediaService.updateSocialMediaService({ data : body,id })
+    successResponseData({
+        res : res,
+        message : `SociaMedia with these ${id} updated successfully`,
+        statusCode : HttpStatus.CREATED,
+        data
+    })
+})
+
+export const deleteSocialMedia = catchAsyncError(async(req,res,next) => {
+    let id = req.params.id
+    let data = await socialmediaService.deleteSocialMediaService({id})
+    successResponseData({
+        res : res,
+        message : `SocialMedia Page with these ${id} deleted successfully`,
+        statusCode : HttpStatus.OK,
+        data
+    })
+})
--- routes/socialmediaRoute.js
+++ routes/socialmediaRoute.js
@@ -7,4 +7,10 @@ socialmediaRouter
 .route("/")
 .post(socialmediaController.createSocialMedia)
 
+socialmediaRouter
+.route("/:id")
+.get(socialmediaController.getSpecifiedSocialMedia)
+.put(socialmediaController.updateSocialMedia)
+.delete(socialmediaController.deleteSocialMedia)
+
 export default socialmediaRouter;
